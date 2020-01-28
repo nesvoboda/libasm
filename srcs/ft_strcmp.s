bfae8745656e08edbe4fdd8ba6c55f6c74f8759f
Arseniy SHISHAEV <ashishae@student.42.fr>
2020-01-28 13:56:06 +0100

    Make strcmp return difference in chars

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -1,18 +1,10 @@
-; ----------------------------------------------------------------------------------------
-; Writes "Hello, World" to the console using only system calls. Runs on 64-bit macOS only.
-; To assemble and run:
-;
-;     nasm -fmacho64 hello.asm && ld hello.o && ./a.out
-; ----------------------------------------------------------------------------------------
-
 			global _ft_strcmp
 			section .text
 looper:		inc rsi
 			inc rdi
 _ft_strcmp: mov cl, byte[rsi]
 			cmp byte[rdi], cl
-			jg greater
-			jl less
+			jne diff
 			cmp byte[rsi], 0
 			jz  end
 			cmp byte[rdi],0
@@ -20,7 +12,6 @@ _ft_strcmp: mov cl, byte[rsi]
 			jmp looper
 end:		mov rax, 0
 			jmp done
-greater:	mov rax, 1
-			jmp done
-less:		mov rax, -1
+diff:		mov al, byte[rdi]
+			sub al, byte[rsi]
 done:		ret
